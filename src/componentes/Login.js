9d0df9dc2b8b6664861df0cd089682dbd095e694
Esteban Salvay Dilena <estebansalvay@imajine.com>
2022-11-08 11:23:03 -0300

    se logro mandar el token por el header

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -6,6 +6,7 @@ import * as yup from 'yup';
 import axios from 'axios';
 
 import Input from './Input';
+import Logged from './Logged';
 
 import '../hojas-estilo/Login.css';
 
@@ -15,6 +16,7 @@ function Login() {
     password: '',
   };
 
+  const [logged, setLogged] = useState(false);
   const [token, setToken] = useState(null);
   const [user, setUser] = useState(null);
 
@@ -22,80 +24,91 @@ function Login() {
 
   const onSubmit = async (values) => {
     const {
-      data: { data: token },
+      data: {
+        data: { token },
+      },
     } = await axios.post('http://localhost:8080/api/auth/sign-in', values);
 
     setToken(token);
-    console.log(token);
+    //console.log(token);
     /*    if (res.data) {
       navigate('/logged');
     }*/
   };
-
   async function buscarMe() {
     try {
-      const res = await axios.get('http://localhost:8080/api/auth/me', {
+      const { data: user } = await axios.get('http://localhost:8080/api/auth/me', {
         headers: { jwt: token },
       });
 
-      setUser(res.data.user);
+      setUser(user);
     } catch (error) {
       console.error(error);
     }
   }
-
   return (
-    <div className='contenedor-login'>
-      <h2>Login</h2>
-      <Formik
-        onSubmit={onSubmit}
-        initialValues={{ ...valores }}
-        validationSchema={yup.object({
-          email: yup.string().email().required(),
-          password: yup.string().min(8).required(),
-        })}
-      >
-        {({ errors, values, isValid, handleChange, handleSubmit }) => {
-          return (
-            <>
-              <Form onSubmit={handleSubmit}>
-                <Input
-                  name='email'
-                  error={errors.email}
-                  value={values.email}
-                  placeholder='Email'
-                  onChange={handleChange}
-                />
-                <Input
-                  name='password'
-                  error={errors.password}
-                  value={values.password}
-                  placeholder='Password'
-                  onChange={handleChange}
-                />
-                <Button type='submit' disabled={!isValid}>
-                  Submit
-                </Button>
-              </Form>
+    <>
+      {logged ? ( //Deberia usar useEffect
+        <Logged />
+      ) : (
+        <div className='contenedor-login'>
+          <h2>Login</h2>
+          <Formik
+            onSubmit={onSubmit}
+            initialValues={{ ...valores }}
+            validationSchema={yup.object({
+              email: yup.string().email().required(),
+              password: yup.string().min(8).required(),
+            })}
+          >
+            {({ errors, values, isValid, handleChange, handleSubmit }) => {
+              return (
+                <>
+                  <Form onSubmit={handleSubmit}>
+                    <Input
+                      name='email'
+                      error={errors.email}
+                      value={values.email}
+                      placeholder='Email'
+                      onChange={handleChange}
+                    />
+                    <Input
+                      name='password'
+                      error={errors.password}
+                      value={values.password}
+                      placeholder='Password'
+                      onChange={handleChange}
+                    />
+                    <Button type='submit' disabled={!isValid}>
+                      Submit
+                    </Button>
+                  </Form>
 
-              <div>
-                <Link className='link' to='/recover'>
-                  Forgot my password
-                </Link>
-              </div>
-              <div>
-                <Link className='link' to='/'>
-                  Go back
-                </Link>
-              </div>
-            </>
-          );
-        }}
-      </Formik>
-
-      <Button onClick={buscarMe}>My Profile</Button>
-      {user ? <div></div> : <div>Debe ser un usuario logeado</div>}
-    </div>
+                  <div>
+                    <Link className='link' to='/recover'>
+                      Forgot my password
+                    </Link>
+                  </div>
+                  <div>
+                    <Link className='link' to='/'>
+                      Go back
+                    </Link>
+                  </div>
+                </>
+              );
+            }}
+          </Formik>
+        </div>
+      )}
+      <div>
+        <Button onClick={buscarMe}>My Profile</Button>
+        {user ? ( //Deberia usar useEffect?
+          <div>{/*Mostrar de alguna forma*/ console.log(user)}</div>
+        ) : (
+          <div>Debe ser un usuario logeado</div>
+        )}
+      </div>
+    </>
   );
 }
 
